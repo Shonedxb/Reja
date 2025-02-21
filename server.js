9d8c1589b577582900cce99c14890bd258fceec0
“Shonedxb” <firdavs.cra@gmail.com>
2025-02-21 04:07:38 +0400

    feat: develope author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+
+    });
 
 //1: Kirish  code
 app.use(express.static("public"));
@@ -16,13 +27,19 @@ app.set("view engine", "ejs");
 
 //4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
+    //TODO: code with db here
+
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
 });
 
 app.get("/", function (req, res) {
     res.render("harid");
 });
+
+
 
 
 const server = http.createServer(app);
