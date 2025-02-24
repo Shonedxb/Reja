cb1013de91342660965ce020ff5bc16caaf10257
“Shonedxb” <firdavs.cra@gmail.com>
2025-02-24 14:35:56 +0400

    Updated train.js

--- train.js
+++ train.js
@@ -1,49 +1,84 @@
-console.log("Jack Ma Maslahatlari");
-const list = [
-    "yahshi talaba boling", //1-20
-    "togri boshliq tanlang va koproq hato qiling", //21-30
-    "ozingizga ishlashni boshlang", //31-40
-    "siz yaxshi bilgan narsalarni bajaring", //41-50
-    "yoshlarga imkoniyat bering", //51-60
-    "endi dam oling, foydasi yoq endi", //61+
-];
 
-async function maslahatBering(a) {
-    if (typeof a !=="number") throw new Error("insert a number");
-    else if (a <= 20) return list[0];
-    else if (a > 20 && a <= 30) return list[1];
-    else if (a > 30 && a <= 40) return list[2];
-    else if (a > 40 && a <= 50) return list[3];
-    else if (a > 50 && a <= 60) return list[4];
-    else {
-        return list[5];
-        // setTimeout(function() {
-        //     return list[5];
-        // }, 5000);
-    }
-}
+/* Asychronous Functions*/
+// // console.log("Jack Ma Maslahatlari");
 
+// // const list = [
+// //     "yaxshi talaba bo'ling", // 0-20
+// //     "to'g'ri bosjliq tanlang va ko'proq xato qiling", // 20-30
+// //     "uzingizga ushlashingizni boshlang", // 30-40
+// //     "siz kuchli bo'lgan narsalarni qiling", // 40-50
+// //     "yoshlarga investitsiya qiling", //
+// //     "endi dam oling, foydasi yo'q endi", // 60
+// // ];
 
-// console.log("passed here 0");
-// maslahatBering(60)
-//     .then((data) => {
-//         console.log("javob:", data);
-//     })
-//     .catch((err) => {
-//         console.log("ERROR:", err);
-
-//     });
-// console.log("passed here 1")
+// // async function maslahatBering(a) {
+// //     if(typeof a !== "number") throw new Error("insert a number", null);
+// //     else if(a <= 20) return list[0];
+// //     else if(a > 20 && a <= 30) return(null, list[1]);
+// //     else if(a > 30 && a <= 40) return(null, list[2]);
+// //     else if(a > 40 && a <= 50) return(null, list[3]);
+// //     else if(a > 50 && a <= 60) return(null, list[4]);
+// //     else {
+// //         // setTimeout(function () {
+// //         //     callback(null, list[5]);
+// //         // }, 5000);
+// //         return list[5];
+// //     }
+// // }
 
 
-//Async / await
+/*Promise functions*/
 
-async function run() {
-    let javob = await maslahatBering(21);
-    console.log(javob);
-    javob = await maslahatBering(31);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);
-}
-run();
+// // console.log("Jack Ma Maslahatlari");
+
+// // const list = [
+// //     "yaxshi talaba bo'ling", // 0-20
+// //     "to'g'ri bosjliq tanlang va ko'proq xato qiling", // 20-30
+// //     "uzingizga ushlashingizni boshlang", // 30-40
+// //     "siz kuchli bo'lgan narsalarni qiling", // 40-50
+// //     "yoshlarga investitsiya qiling", //
+// //     "endi dam oling, foydasi yo'q endi", // 60
+// // ];
+
+//ASYNC function
+// // Async function maslahatBering(a) {
+// //     if(typeof a !== "number") throw new Error("insert a number", null);
+// //     else if(a <= 20) return list[0];
+// //     else if(a > 20 && a <= 30) return(null, list[1]);
+// //     else if(a > 30 && a <= 40) return(null, list[2]);
+// //     else if(a > 40 && a <= 50) return(null, list[3]);
+// //     else if(a > 50 && a <= 60) return(null, list[4]);
+// //     else {
+// //             // setTimeout(function () {
+// //             //     callback(null, list[5]);
+// //             // }, 5000);
+// //             // return list[5];
+// //         return new Promise((resolve, reject) => {
+// //             setTimeout(() => {
+// //                 resolve(list[5]);
+// //             }, 5000);
+// //         });
+// //     }
+// // };
+
+// // // then/catch
+// // console.log("passed here 0");
+// // maslahatBering(51).then((data) => {
+// //     console.log("Javob: ", data);
+// // }).catch((err) => {
+// //     console.log("Error:", err);
+// // });
+// // console.log("passed here 1");
+
+
+// // // asynch/await
+// // async function run() {
+// //     let javob = await maslahatBering(25);
+// //     console.log(javob);
+// //     javob = await maslahatBering(70);
+// //     console.log(javob);
+// //     javob = await maslahatBering(41);
+// //     console.log(javob);
+// // }
+// // run();
+
