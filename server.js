66f861253fa68f1d5a059531df634429b757bff5
“Shonedxb” <firdavs.cra@gmail.com>
2025-02-28 08:24:57 +0400

    feat MongoDB ulanish

--- server.js
+++ server.js
@@ -1,49 +1,57 @@
-console.log("web serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
-    }
+let db;
+const connectionString = "mongodb://atlas-sql-67bef2171e685c7721bca43c-lk0n8.a.query.mongodb.net/Reja?ssl=true&authSource=admin"
 
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    }, (err, client) => {
+        if (err) console.log("Error connecting to MongoDB");
+        else {
+            console.log("MongoDB Connection successful");
+            module.exports = client;
+
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function () {
+                console.log(
+                    `The server is running succesfully on port ${PORT}, http://localhost:${PORT}`
+                );
+            });
+        }
+    } 
+);
+
+
+
+
+/*
+
+const http = require("http");
+const { MongoClient } = require("mongodb");
+
+const connectionString = "mongodb://atlas-sql-67bef2171e685c7721bca43c-lk0n8.a.query.mongodb.net/Reja?ssl=true&authSource=admin";
+
+const client = new MongoClient(connectionString);
+
+client.connect()
+  .then(() => {
+    console.log("MongoDB Connection successful");
+    const app = require("./app");
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function () {
+      console.log(
+        `The server is running succesfully on port ${PORT}, http://localhost:${PORT}`
+      );
     });
+  })
+  .catch(err => {
+    console.log("Error connecting to MongoDB:", err);
+  });
 
-//1: Kirish  code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-//2 Session Code
-
-//3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4 Routing code
-app.post("/create-item", (req, res) => {
-    //TODO: code with db here
-
-});
-
-app.get('/author', (req, res) => {
-    res.render("author", {user: user});
-});
-
-app.get("/", function (req, res) {
-    res.render("reja");
-});
-
-
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
-    });
+  */
