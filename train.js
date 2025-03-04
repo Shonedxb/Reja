87035894c67a13f7072d831dac23e03a1965081a
“Shonedxb” <firdavs.cra@gmail.com>
2025-03-05 02:47:49 +0400

    feat:update & Task D

--- train.js
+++ train.js
@@ -52,7 +52,7 @@
 
 
 
-// // C-TASK STARTS
+////////////////////////////////// C-TASK starts //////////////////////////////////
 
     // Shunday function tuzing, u 2ta string parametr ega bolsin, hamda //
     // agar har ikkala string bir hil harflardan iborat bolsa true aks //
@@ -80,7 +80,97 @@
 // console.log(result);
 
 
-//   // C-TASK ENDS
+////////////////////////////////// C-TASK ENDS //////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////// D-TASK starts //////////////////////////////////
+
+// Shunday class tuzing tuzing nomi Shop, 
+// va uni constructoriga 3 hil mahsulot pass bolsin, 
+// hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. 
+// Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 
+// 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() 
+// return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+// let now = new Date();
+// // let hour = now.getHours();
+// // let minute = now.getMinutes();
+// // minute = minute < 10 ? "0" + minute : minute;
+
+
+// // class Shop {
+// //   constructor(lagmon, non, cola) {
+// //     this.lagmon = lagmon;
+// //     this.non = non;
+// //     this.cola = cola;
+// //   }
+
+// //   sotish(items, amount) {
+// //     // lag'mon
+// //     if (items === "lagmon") {
+// //       if (this.lagmon >= amount) {
+// //         this.lagmon -= amount;
+// //       }
+// //     }
+
+// //     // non
+// //     if (items === "non") {
+// //       if (this.non >= amount) {
+// //         this.non -= amount;
+// //       }
+// //     }
+
+// //     // cola
+
+// //     if (items === "cola") {
+// //       if (this.cola >= amount) {
+// //         this.cola -= amount;
+// //       }
+// //     }
+
+// //     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
+// //   }
+
+// //   qabul(items, amount) {
+// //     // lagmon
+// //     if (items === "lagmon") {
+// //       this.lagmon += amount;
+// //     }
+
+// //     // non
+// //     if (items === "non") {
+// //       this.non += amount;
+// //     }
+
+// //     // cola
+// //     if (items === "cola") {
+// //       this.cola += amount;
+// //     }
+// //     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
+// //   }
+
+// //   qoldiq() {
+// //     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola qolgan!`;
+// //   }
+// // }
+
+// // const trade = new Shop(6, 2, 4);
+// // console.log(trade.sotish("cola", 2));
+// // console.log(trade.qabul("non", 2));
+
+////////////////////////////////// C-TASK ENDS //////////////////////////////////
+
+
+
 
 
 
