8185144a6f81329c62c95781b242af1c93677b43
“Shonedxb” <firdavs.cra@gmail.com>
2025-03-12 09:34:41 +0400

    G-TASK

--- train.js
+++ train.js
@@ -177,7 +177,7 @@
 
 
 
-////////////////////////////////// E-TASK ENDS //////////////////////////////////
+////////////////////////////////// E-TASK STARTS //////////////////////////////////
 
 // Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
 // MASALAN: getReverse("hello") return qilsin "olleh"
@@ -197,6 +197,40 @@
 // console.log("result2:", result2);
 
 ////////////////////////////////// E-TASK ENDS //////////////////////////////////
+
+
+
+
+////////////////////////////////// G-TASK STARTS //////////////////////////////////
+
+// Shunday function tuzingki unga integerlardan iborat array pass 
+// bolsin va function bizga osha arrayning eng katta qiymatiga 
+// tegishli birinchi indexni qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+
+// function getHighestIndex(arr) {
+//     // defining new variable
+//     let greatest;
+//     let greatestIndex;
+//     // looping through the arr parameter of function
+//     for (let i = 0; i < arr.length; i++) {
+//       // giving the condition if a found number is not the greatest
+//       // in that case arr[i] is greater than that.
+//       if (!greatest || arr[i] > greatest) {
+//         greatest = arr[i];
+//         // if not the greatestIndex = looped array of i
+//         greatestIndex = i;
+//       }
+//     }
+//     return greatestIndex;
+//   }
+//   let array = [5, 21, 12, 21, 8];
+//   const result = getHighestIndex(array);
+//   console.log("result:", result);
+
+
+////////////////////////////////// G-TASK ENDS //////////////////////////////////
+
 
 
 
